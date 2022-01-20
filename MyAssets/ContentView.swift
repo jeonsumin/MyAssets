78d7b71cc941c69f1e23cbb1834411a936fa5b58
joensumin <soomis7@naver.com>
2022-01-20 09:37:02 +0900

    Initial Commit

---
+++ MyAssets/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyAssets
+//
+//  Created by Terry on 2022/01/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyAssets/MyAssetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAssetsApp.swift
+//  MyAssets
+//
+//  Created by Terry on 2022/01/20.
+//
+
+import SwiftUI
+
+@main
+struct MyAssetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
