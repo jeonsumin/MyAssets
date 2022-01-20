210118201e82739919e8c1c51e274ede63bcff05
joensumin <soomis7@naver.com>
2022-01-20 09:44:18 +0900

    SwiftUI 를 이용하여 구현하기

--- MyAssets/ContentView.swift
+++ MyAssets/ContentView.swift
@@ -8,9 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection: Tap = .asset
+    
+    enum Tap {
+        case asset
+        case recommend
+        case alert
+        case setting
+    }
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView(selection: $selection){
+            Color.white
+                .tabItem {
+                    Image(systemName: "dollarsign.circle.fill")
+                    Text("자산")
+                }
+                .tag(Tap.asset)
+            Color.blue
+                .tabItem {
+                    Image(systemName: "hand.thumbsup.fill")
+                    Text("추천")
+                }
+                .tag(Tap.recommend)
+            Color.yellow
+                .tabItem {
+                    Image(systemName: "bell.fill")
+                    Text("알림")
+                }
+                .tag(Tap.alert)
+            Color.red
+                .tabItem {
+                    Image(systemName: "gearshape.fill")
+                    Text("설정")
+                }
+                .tag(Tap.setting)
+        }
     }
 }
 
